10a88758b97c858ecd48d86c1d53e87a905eff08
jurgengalicia <jurgen.galicia@gmail.com>
2018-09-11 15:01:35 -0700

    Initial Commit
    created a main for testing purposes.

---
+++ Makefile
@@ -0,0 +1,6 @@
+default:
+	gcc -g main.c
+	./a.exe
+	
+d:
+	rm -f a.exe a.exe.stackdump 
---
+++ main.c
@@ -0,0 +1,10 @@
+#include "f0700.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int arr1[1408] = {1,2,3};
+	int arr2[116] = {0};
+	printf("%d", f0700(64,arr1,3,arr2));
+	return 0;
+}
